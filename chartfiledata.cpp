7d3ca056e5ec6ab4542245d6a927bfd455cb7570
Perfect_03 <zlobin_gg52926@mail.ru>
2022-06-22 14:20:40 +0700

    add comments in chartfiledata.cpp

--- chartfiledata.cpp
+++ chartfiledata.cpp
@@ -1,28 +1,28 @@
 #include "chartfiledata.h"
 #include <QMessageBox>
 
-DataTable ChartFileDataSqlite::getData (QString path)
+DataTable ChartFileDataSqlite::getData (QString path) // получение данных из sql
 {
-    DataTable data;
-    DataList dataList;
+    DataTable data; // данные
+    DataList dataList; // таблица данных, на основании которой будем строить график
     qreal yValue(0);
     QSqlDatabase dbase = QSqlDatabase::addDatabase("QSQLITE");
-    dbase.setDatabaseName(path);
+    dbase.setDatabaseName(path); // записываем данные из файла
 
-    if (!dbase.open()) {
-        QMessageBox msg;
+    if (!dbase.open()) { // исключение
+        QMessageBox msg; // окно с текстом об ошибке
         msg.setText("База данных не открыта." + path);
-        msg.exec();
+        msg.exec(); // функцией exec отображаем его
     }
 
     else
     {
         QSqlQuery query("SELECT * FROM " + dbase.tables().takeFirst());
-            while (query.next()) {
-                QString name = query.value(0).toString();
-                QPointF salary(query.value(1).toDouble(),rand());
-                dataList << Data(salary, name);
-                qDebug() << "name is " << name << ". salary is " << salary;
+            while (query.next()) { // цикл по элементам БД
+                QString name = query.value(0).toString(); // название элемента
+                QPointF salary(query.value(1).toDouble(),rand()); // рандомное значение, оно не пригодится
+                dataList << Data(salary, name); // запись данных оператором <<
+                qDebug() << "name is " << name << ". salary is " << salary; // текст вывода
             }
     }
 
@@ -30,27 +30,27 @@ DataTable ChartFileDataSqlite::getData (QString path)
    return data;
 }
 
-DataTable ChartFileDataJson::getData(QString path)
+DataTable ChartFileDataJson::getData(QString path) // получение данных из json
 {
-    DataTable data;
-    DataList dataList;
+    DataTable data; // данные
+    DataList dataList; // таблица данных, на основании которой будем строить график
     QString key;
     QFile file;
-    file.setFileName(path);
-    file.open(QIODevice::ReadOnly | QIODevice::Text);
-    key = file.readAll();
-    file.close();
+    file.setFileName(path); // читаем файл
+    file.open(QIODevice::ReadOnly | QIODevice::Text);// открываем файл на чтение
+    key = file.readAll(); // сначала читаем все элементы
+    file.close(); // и сразу закрываем файл
     QJsonDocument doc = QJsonDocument::fromJson(key.toUtf8());
     QJsonObject jsonObject = doc.object();
-    QJsonArray jsonArray = jsonObject["12"].toArray();
-    foreach (const QJsonValue & value, jsonArray)
+    QJsonArray jsonArray = jsonObject["11"].toArray(); // формируем массив объектов
+    foreach (const QJsonValue & value, jsonArray) // и проходим по нему
     {
-        if (value.isObject())
+        if (value.isObject()) // если объект существует, считываем данные
         {
             QJsonObject obj = value.toObject();
             QString name = obj["key"].toString();
             QPointF salary(obj["ID"].toInt(),rand());
-            dataList << Data(salary, name);
+            dataList << Data(salary, name); // запись в таблицу данных
         }
     }
     //std::sort (data.begin(), data.end());
